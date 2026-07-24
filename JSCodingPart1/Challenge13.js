aa911ddf837c01417319099cba04e314f20cc8f0
Kiran Potdar <kiranpotdar619@gmail.com>
2026-07-24 21:14:59 +0530

    Add readiness check function for JS basics

---
+++ JSCodingPart1/Challenge13.js
@@ -0,0 +1,8 @@
+function checkJsBasicsReadinessGate(hasSetup, hasPractice, hasNotes) {
+  // write your code here
+  if (hasSetup == true && hasPractice == true && hasNotes == true) {
+    return "READY";
+  }
+  else
+    return "BLOCKED";
+}
