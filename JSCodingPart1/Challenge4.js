2ae8eda9e888c27a6720f8f98e399b74f63e9e35
Kiran Potdar <kiranpotdar619@gmail.com>
2026-07-24 21:10:01 +0530

    Implement getJsBasicsKeywordMeaning function

---
+++ JSCodingPart1/Challenge4.js
@@ -0,0 +1,11 @@
+function getJsBasicsKeywordMeaning(term) {
+  // write your code here
+  if (term == "NODE")
+    return "runtime";
+  else if (term == "npm")
+    return "package-manager";
+  else if (term == " v8 ")
+    return "engine";
+  else
+    return "unknown";
+}
