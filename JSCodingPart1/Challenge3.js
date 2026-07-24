472b9a2fd6ce9ec5b2cb063dcee38e16a5f38d62
Kiran Potdar <kiranpotdar619@gmail.com>
2026-07-24 15:36:34 +0000

    Pending changes exported from your codespace

---
+++ JSCodingPart1/Challenge3.js
@@ -0,0 +1,12 @@
+let score=90;
+function classifyJsBasicsScore(score) {
+  // write your code here
+  if (score >= 90)
+    return "EXCELLENT";
+  else if (score >= 75 && score < 90)
+    return "GOOD";
+    else if (score >= 50 && score < 75)
+    return "NEEDS_PRACTICE";
+  else if (score < 50)
+    return "REVISIT";
+}
